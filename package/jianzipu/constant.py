f80df61f939133ebc3272245d2499398266d6d5b
alephpi <maosicheng98@gmail.com>
2023-08-07 23:17:28 +0200

    feat: rewrite parsing logic with pyparsing

--- package/jianzipu/constant.py
+++ package/jianzipu/constant.py
@@ -1,54 +1,57 @@
 import re
 from functools import reduce
-import operator
 from typing import Dict
+import operator
 import yaml
+from pyparsing import Group, Opt, ParserElement, ZeroOrMore, Regex, oneOf
+from jianzipu.constant import *
+
 
 # fingers, modifiers and markers are open, so we import from outside
 with open('./jianzipu/data.yaml', 'r', encoding='utf-8') as f:
   d: Dict[str, Dict[str, str]] = yaml.safe_load(f)
-
 # 保证字符长的在前（如防止勾剔只匹配了勾）
-hui_finger = '|'.join(list(sorted(d['徽位指法'].keys(), key=len, reverse=True)))
-xian_finger = '|'.join(list(sorted(d['弦序指法'].keys(), key=len, reverse=True)))
-move_finger = '|'.join(list(sorted(d['走位指法'].keys(), key=len, reverse=True)))
-special_finger = '|'.join(list(sorted(d['特殊指法'].keys(), key=len, reverse=True)))
-modifier = '|'.join(list(sorted(d['修饰'].keys(), key=len, reverse=True)))
-both_finger = '|'.join(list(sorted(d['联袂指法'].keys(), key=len, reverse=True)))
-complex_finger = '|'.join(list(sorted(d['复式指法'].keys(), key=len, reverse=True)))
-marker = '|'.join(list(sorted(d['记号'].keys(), key=len, reverse=True)))
+HUI_FINGER = sorted(d['徽位指法'].keys(), key=len, reverse=True)
+XIAN_FINGER = sorted(d['弦序指法'].keys(), key=len, reverse=True)
+MOVE_FINGER = sorted(d['走位指法'].keys(), key=len, reverse=True)
+SPECIAL_FINGER = sorted(d['特殊指法'].keys(), key=len, reverse=True)
+MODIFIER = sorted(d['修饰'].keys(), key=len, reverse=True)
+BOTH_FINGER = sorted(d['联袂指法'].keys(), key=len, reverse=True)
+COMPLEX_FINGER = sorted(d['复式指法'].keys(), key=len, reverse=True)
+MARKER = sorted(d['记号'].keys(), key=len, reverse=True)
 
 # numbers are closed, so we list here
-# XIAN_DOMAIN = ['一弦','二弦','三弦','四弦','五弦','六弦','七弦']
-# HUI_DOMAIN = ['一徽','二徽','三徽','四徽','五徽','六徽','七徽','八徽','九徽','十徽','十一徽','十二徽','十三徽','徽外']
-# FEN_DOMAIN = ['一分','二分','三分','四分','五分','六分','七分','八分','九分','半']
+XIAN = ['一弦','二弦','三弦','四弦','五弦','六弦','七弦']
+HUI = ['十一徽','十二徽','十三徽','一徽','二徽','三徽','四徽','五徽','六徽','七徽','八徽','九徽','十徽']
+FEN = ['一分','二分','三分','四分','五分','六分','七分','八分','九分','半']
 
-# BN-form see readme.md
+# 我们禁止谱字读法中间出现空格，空格用于分割
+ParserElement.set_default_whitespace_chars('')
+# Define individual components
+hui = (ZeroOrMore((oneOf(HUI) + Opt(oneOf(FEN))) | '徽外')).set_results_name('hui')
+xian = (ZeroOrMore(oneOf(XIAN))).set_results_name('xian')
+hui_finger = oneOf(HUI_FINGER).set_results_name('hui_finger')
+xian_finger = oneOf(XIAN_FINGER).set_results_name('xian_finger')
+move_finger = oneOf(MOVE_FINGER).set_results_name('move_finger')
+special_finger = oneOf(SPECIAL_FINGER).set_results_name('special_finger')
+modifier = oneOf(MODIFIER).set_results_name('modifier')
+both_finger = oneOf(BOTH_FINGER).set_results_name('both_finger')
+complex_finger = oneOf(COMPLEX_FINGER).set_results_name('complex_finger')
+marker = oneOf(MARKER).set_results_name('marker')
 
-## symbol pattern
-XIAN = r'(?P<xian>(一弦|二弦|三弦|四弦|五弦|六弦|七弦)*)'
-HUI = r'(?P<hui>((十一徽|十二徽|十三徽|一徽|二徽|三徽|四徽|五徽|六徽|七徽|八徽|九徽|十徽)(一分|二分|三分|四分|五分|六分|七分|八分|九分|半)?|徽外)*)'
-HUI_FINGER = fr'(?P<hui_finger>{hui_finger})'
-XIAN_FINGER = fr'(?P<xian_finger>{xian_finger})'
-MOVE_FINGER = fr'(?P<move_finger>{move_finger})'
-SPECIAL_FINGER = fr'(?P<special_FINGER>{special_finger})'
-MODIFIER = fr'(?P<modifier>{modifier})'
-BOTH_FINGER = fr'(?P<both_finger>{both_finger})'
-COMPLEX_FINGER = fr'(?P<complex_finger>{complex_finger})'
-MARKER = fr'(?P<marker>{marker})'
+# Define phrase patterns
+hui_finger_phrase = Group(hui_finger + hui).set_results_name('hui_finger_phrase')
+xian_finger_phrase = Group(xian_finger + xian).set_results_name('xian_finger_phrase')
+left_sub_phrase = Group(hui_finger_phrase + Opt(special_finger) + xian).set_results_name('left_sub_phrase')
+right_sub_phrase = Group(hui_finger_phrase + Opt(special_finger) + xian).set_results_name('right_sub_phrase')
 
-## phrase pattern
-HUI_FINGER_PHRASE = fr'(?P<hui_finger_phrase>{HUI_FINGER}{HUI})'
-XIAN_FINGER_PHRASE = fr'(?P<xian_finger_phrase>{XIAN_FINGER}{XIAN})'
+# Define form pattern
+simple_form = Group(Opt(hui_finger_phrase) + Opt(special_finger) + Opt(xian_finger_phrase)).set_results_name('simple_form')
+complex_form = Group(complex_finger + left_sub_phrase + right_sub_phrase).set_results_name('complex_form')
+aside_form = Group(Opt(modifier) + move_finger + hui).set_results_name('aside_form')
 
-## form pattern
-SIMPLE_FORM = fr'(?P<common_form>{HUI_FINGER_PHRASE}?{SPECIAL_FINGER}?{XIAN_FINGER_PHRASE}?)'
-COMPLEX_FORM = fr'(?P<complex_form>{COMPLEX_FINGER}{HUI_FINGER_PHRASE}{SPECIAL_FINGER}?{XIAN}{HUI_FINGER_PHRASE}{SPECIAL_FINGER}?{XIAN})'
-ASIDE_FORM = fr'(?P<aside_form>{MODIFIER}*{MOVE_FINGER}+{HUI}*)'
-
-
-# 谱字
-PUZI = re.compile(fr'{SIMPLE_FORM}|{COMPLEX_FORM}|{ASIDE_FORM}|{MARKER}')
+# 谱字, lazy matching, order is important
+PUZI = marker | both_finger | aside_form | complex_form | simple_form  
 
 # IDC
 class IDC:
