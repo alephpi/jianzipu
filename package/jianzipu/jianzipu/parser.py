671fc176784cd0f90e7bb7bf408f32938e82bd26
alephpi <maosicheng98@gmail.com>
2023-09-18 09:20:15 +0800

    feat: support parsing abbreivation form

--- package/jianzipu/jianzipu/parser.py
+++ package/jianzipu/jianzipu/parser.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from pyparsing import Group, Opt, ParserElement, ZeroOrMore, oneOf
 
 from .grammar import Note
@@ -14,15 +15,15 @@ BOTH_FINGER = sorted(d['联袂指法'].keys(), key=len, reverse=True)
 COMPLEX_FINGER = sorted(d['复式指法'].keys(), key=len, reverse=True)
 MARKER = sorted(d['记号'].keys(), key=len, reverse=True)
 
-# numbers are closed, so we list here
-XIAN = ['一弦','二弦','三弦','四弦','五弦','六弦','七弦']
-HUI = ['十一徽','十二徽','十三徽','一徽','二徽','三徽','四徽','五徽','六徽','七徽','八徽','九徽','十徽']
-FEN = ['一分','二分','三分','四分','五分','六分','七分','八分','九分','半']
 
 # white space is forbidden inside a note (but as a seperator between them)
 ParserElement.set_default_whitespace_chars('')
 
-class ParseVar:
+class OrthoParseVar:
+  # numbers are closed, so we list here
+  XIAN = ['一弦','二弦','三弦','四弦','五弦','六弦','七弦']
+  HUI = ['十一徽','十二徽','十三徽','一徽','二徽','三徽','四徽','五徽','六徽','七徽','八徽','九徽','十徽']
+  FEN = ['一分','二分','三分','四分','五分','六分','七分','八分','九分','半']
   # Define individual components
   hui = (ZeroOrMore((oneOf(HUI) + Opt(oneOf(FEN))) | '徽外')).set_results_name('number')
   xian = (ZeroOrMore(oneOf(XIAN))).set_results_name('number')
@@ -61,9 +62,60 @@ class ParseVar:
   # 谱字, lazy matching, order is important
   PUZI = complex_form | marker | both_finger | aside_form | simple_form
 
-def parse(s: str) -> Note:
-  d = ParseVar.PUZI.parse_string(s).as_dict()
+class AbbrParseVar:
+  # numbers are closed, so we list here
+  XIAN = ['一','二','三','四','五','六','七']
+  HUI = ['十一','十二','十三','一','二','三','四','五','六','七','八','九','十']
+  FEN = ['一','二','三','四','五','六','七','八','九','半']
+  HUI_FINGER = ['大','食','中','名','跪','散']
+  # Define individual components
+  hui = (ZeroOrMore((oneOf(HUI) + Opt(oneOf(FEN))) | '外')).set_results_name('number')
+  xian = (ZeroOrMore(oneOf(XIAN))).set_results_name('number')
+  hui_finger = oneOf(HUI_FINGER).set_results_name('finger')
+  xian_finger = oneOf(XIAN_FINGER).set_results_name('finger')
+  move_finger = oneOf(MOVE_FINGER).set_results_name('finger')
+  special_finger = oneOf(SPECIAL_FINGER).set_results_name('special_finger')
+  modifier = oneOf(MODIFIER).set_results_name('modifier')
+  both_finger = oneOf(BOTH_FINGER).set_results_name('both_finger')
+  complex_finger = oneOf(COMPLEX_FINGER).set_results_name('complex_finger')
+  marker = oneOf(MARKER).set_results_name('marker')
 
+  # Define phrase patterns
+  hui_finger_phrase = Group(hui_finger + hui).set_results_name('hui_finger_phrase')
+  xian_finger_phrase = Group(xian_finger + xian).set_results_name('xian_finger_phrase')
+
+  # reduced_xian_finger phrase, which is a simplified version of xian_finger_phrase
+  # only used in complex from
+  # since the complex_finger is actually the xian_finger 
+  reduced_xian_finger_phrase = Group(xian).set_results_name('xian_finger_phrase')
+  left_sub_phrase = Group(hui_finger_phrase + Opt(special_finger) + reduced_xian_finger_phrase).set_results_name('left_sub_phrase')
+  right_sub_phrase = Group(hui_finger_phrase + Opt(special_finger) + reduced_xian_finger_phrase).set_results_name('right_sub_phrase')
+
+  # move_finger_phrase is a similar to hui_finger_phrase, used in aside form
+  move_finger_phrase = Group(move_finger + hui).set_results_name('move_finger_phrase')
+
+
+  # Define form pattern
+  # simple form must have xian_finger_phrase, while hui_finger_phrase is optional
+  simple_form = Group(Opt(hui_finger_phrase) + Opt(special_finger) + xian_finger_phrase).set_results_name('simple_form')
+  # complex form must have all
+  complex_form = Group(complex_finger + left_sub_phrase + right_sub_phrase).set_results_name('complex_form')
+  # similar for aside_form
+  aside_form = Group(Opt(modifier) + Opt(special_finger) + move_finger_phrase).set_results_name('aside_form')
+
+  # 谱字, lazy matching, order is important
+  PUZI = complex_form | marker | both_finger | aside_form | simple_form
+
+
+def parse(s: str, form = Literal['abbr','ortho']) -> Note:
+  match form:
+    case 'abbr':
+      d = AbbrParseVar.PUZI.parse_string(s).as_dict()
+    case 'ortho':
+      d = OrthoParseVar.PUZI.parse_string(s).as_dict()
+    case _:
+      raise ValueError(f"form must be either 'abbr'or 'ortho'")
+  print(d)
   # # linting
   # match xian_finger:
   #   case '历':
@@ -104,6 +156,13 @@ def transcribe(s: str) -> str:
     '外': '徽外',
   }
 
+  # 撮式
+  if s[0] in COMPLEX_FINGER:
+
+    left = s[:i+2]
+    right = s[i+2:]
+  # 旁注形式
+  # 简单形式
   # 添加徽分弦
   for finger in XIAN_FINGER:
     i = s.find(finger)
@@ -141,5 +200,3 @@ def transcribe(s: str) -> str:
       break
   
   return t
-
-  
--- package/test.py
+++ package/test.py
@@ -1,4 +1,4 @@
-from jianzipu.jianzipu.parser import parse
+from jianzipu.jianzipu.parser import parse, transcribe
 
 def test_simple_form_parse_and_str():
   l = [
@@ -16,14 +16,42 @@ def test_simple_form_parse_and_str():
       '中指徽外历二弦三弦',
       '散音历二弦三弦',
       '大指七徽六分掩',
-      '名指七徽六分掐起',
+      '名指七徽六分搯起',
       # '打圆',
       # '不动',
       # '推出',
       # '滚拂',
       ]
   for i in l:
-    assert str(parse(i)) == i
+    assert str(parse(i, form='ortho')) == i
+
+def test_abbr_simple_form_parse_and_str():
+  l = [
+      '大一六勾三',
+      '大二剔四',
+      '跪三七抹五',
+      '跪四挑六',
+      '名五八注托七',
+      '名六注擘一',
+      '中七打二',
+      '中八九摘三',
+      '食十绰打四',
+      '食外绰打五',
+      '中外摘六',
+      '中外历二三',
+      '散历二三',
+      '大七六掩',
+      '名七六搯起',
+      '名十二搯起',
+      '名十二三搯起',
+      # '打圆',
+      # '不动',
+      # '推出',
+      # '滚拂',
+      ]
+
+  for i in l:
+    assert str(parse(i, form='abbr')) == i
 
 def test_complex_form_parse_and_str():
   l = [
@@ -35,7 +63,19 @@ def test_complex_form_parse_and_str():
       '撮大指徽外七弦散音六弦',
       ]
   for i in l:
-    assert str(parse(i)) == i
+    assert str(parse(i, form='ortho')) == i
+
+def test_abbr_complex_form_parse_and_str():
+  l = [
+      '撮大七七散六',
+      '撮大七七名六二五',
+      '撮大五六名六二五',
+      '撮大三六四散五',
+      '撮大五七散六',
+      '撮大外七散六',
+      ]
+  for i in l:
+    assert str(parse(i, form='abbr')) == i
 
 def test_aside_form_parse_and_str():
   l = [
@@ -56,7 +96,28 @@ def test_aside_form_parse_and_str():
       '急撞',
       ]
   for i in l:
-    assert str(parse(i)) == i
+    assert str(parse(i, form='ortho')) == i
+
+def test_abbr_aside_form_parse_and_str():
+  l = [
+      '上七六',
+      '下五四',
+      '进',
+      '退',
+      '进复',
+      '退复',
+      '慢上七六',
+      '紧下五四',
+      '引上七六',
+      '淌下五四',
+      '注上七六',
+      '绰下五四',
+      '注吟',
+      '绰猱',
+      '急撞',
+      ]
+  for i in l:
+    assert str(parse(i, form='abbr')) == i
 
 def test_marker_form_parse_and_str():
   l = [
@@ -71,7 +132,57 @@ def test_marker_form_parse_and_str():
       '操终',
       ]
   for i in l:
-    assert str(parse(i)) == i
+    assert str(parse(i, form='ortho')) == i
+
+
+def test_transcribe():
+
+  abbr = [
+      '大一六勾三',
+      '大二剔四',
+      '跪三七抹五',
+      '跪四挑六',
+      '名五八注托七',
+      '名六注擘一',
+      '中七打二',
+      '中八九摘三',
+      '食十绰打四',
+      '食外绰打五',
+      '中外摘六',
+      '中外历二三',
+      '散历二三',
+      '大七六掩',
+      '名七六搯起',
+      '打圆',
+      '不动',
+      '推出',
+      '滚拂',
+      ]
+
+  ortho = [
+      '大指一徽六分勾三弦',
+      '大指二徽剔四弦',
+      '跪指三徽七分抹五弦',
+      '跪指四徽挑六弦',
+      '名指五徽八分注托七弦',
+      '名指六徽注擘一弦',
+      '中指七徽打二弦',
+      '中指八徽九分摘三弦',
+      '食指十徽绰打四弦',
+      '食指徽外绰打五弦',
+      '中指徽外摘六弦',
+      '中指徽外历二弦三弦',
+      '散音历二弦三弦',
+      '大指七徽六分掩',
+      '名指七徽六分搯起',
+      '打圆',
+      '不动',
+      '推出',
+      '滚拂',
+      ]
+
+  for i, j in zip(abbr, ortho):
+    assert str(transcribe(i)) == j
 
 def test_char():
   pass
