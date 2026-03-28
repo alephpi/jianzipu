c067275cead41aeb32835ce3c8557b1cb1f3e3a0
alephpi <maosicheng98@gmail.com>
2026-03-28 11:31:02 +0100

    feat: new parse model

--- jianzipu/parser.py
+++ jianzipu/parser.py
@@ -1,10 +1,9 @@
-import pprint as pp
-from typing import Literal
+from dataclasses import dataclass, field
+from typing import List, Literal
 
 from pyparsing import Group, Opt, ParserElement, ZeroOrMore, one_of
 
 from .constant import d
-from .grammar import Note
 
 # fingers, modifiers and markers are open, so we import from outside
 # 保证字符长的在前（如防止勾剔只匹配了勾）
@@ -17,7 +16,6 @@ BOTH_FINGER = sorted(d['联袂指法'].keys(), key=len, reverse=True)
 COMPLEX_FINGER = sorted(d['复式指法'].keys(), key=len, reverse=True)
 MARKER = sorted(d['记号'].keys(), key=len, reverse=True)
 
-
 # white space is forbidden inside a note (but as a seperator between them)
 ParserElement.set_default_whitespace_chars('')
 
@@ -27,39 +25,39 @@ class OrthoParseVar:
   HUI = ['十一徽','十二徽','十三徽','一徽','二徽','三徽','四徽','五徽','六徽','七徽','八徽','九徽','十徽']
   FEN = ['一分','二分','三分','四分','五分','六分','七分','八分','九分','半']
   # Define individual components
-  hui = (ZeroOrMore((one_of(HUI) + Opt(one_of(FEN))) | '徽外')).set_results_name('number')
-  xian = (ZeroOrMore(one_of(XIAN))).set_results_name('number')
-  hui_finger = one_of(HUI_FINGER).set_results_name('finger')
-  xian_finger = one_of(XIAN_FINGER).set_results_name('finger')
-  move_finger = one_of(MOVE_FINGER).set_results_name('finger')
-  special_finger = one_of(SPECIAL_FINGER).set_results_name('special_finger')
-  modifier = one_of(MODIFIER).set_results_name('modifier')
-  both_finger = one_of(BOTH_FINGER).set_results_name('both_finger')
-  complex_finger = one_of(COMPLEX_FINGER).set_results_name('complex_finger')
+  hui = (ZeroOrMore((one_of(HUI) + Opt(one_of(FEN))) | '徽外')).set_results_name('hn')
+  xian = (ZeroOrMore(one_of(XIAN))).set_results_name('xn')
+  hui_finger = one_of(HUI_FINGER).set_results_name('hf')
+  xian_finger = one_of(XIAN_FINGER).set_results_name('xf')
+  move_finger = one_of(MOVE_FINGER).set_results_name('mf')
+  special_finger = one_of(SPECIAL_FINGER).set_results_name('sf')
+  modifier = one_of(MODIFIER).set_results_name('mod')
+  complex_finger = one_of(COMPLEX_FINGER).set_results_name('cf')
+  both_finger = one_of(BOTH_FINGER).set_results_name('bf')
   marker = one_of(MARKER).set_results_name('marker')
 
   # Define phrase patterns
-  hui_finger_phrase = Group(hui_finger + hui).set_results_name('hui_finger_phrase')
-  xian_finger_phrase = Group(xian_finger + xian).set_results_name('xian_finger_phrase')
+  hui_finger_phrase = Group(hui_finger + hui).set_results_name('hfp')
+  xian_finger_phrase = Group(xian_finger + xian).set_results_name('xfp')
 
   # reduced_xian_finger phrase, which is a simplified version of xian_finger_phrase
-  # only used in complex from
+  # only used in complex form
   # since the complex_finger is actually the xian_finger 
-  reduced_xian_finger_phrase = Group(xian).set_results_name('xian_finger_phrase')
-  left_sub_phrase = Group(hui_finger_phrase + Opt(special_finger) + reduced_xian_finger_phrase).set_results_name('left_sub_phrase')
-  right_sub_phrase = Group(hui_finger_phrase + Opt(special_finger) + reduced_xian_finger_phrase).set_results_name('right_sub_phrase')
+  reduced_xian_finger_phrase = Group(xian).set_results_name('xfp')
+  left_sub_phrase = Group(hui_finger_phrase + Opt(special_finger) + reduced_xian_finger_phrase).set_results_name('lsp')
+  right_sub_phrase = Group(hui_finger_phrase + Opt(special_finger) + reduced_xian_finger_phrase).set_results_name('rsp')
 
   # move_finger_phrase is a similar to hui_finger_phrase, used in aside form
-  move_finger_phrase = Group(move_finger + hui).set_results_name('move_finger_phrase')
+  move_finger_phrase = Group(move_finger + hui).set_results_name('mfp')
 
 
   # Define form pattern
   # simple form must have xian_finger_phrase, while hui_finger_phrase is optional
-  simple_form = Group(Opt(hui_finger_phrase) + Opt(special_finger) + xian_finger_phrase).set_results_name('simple_form')
+  simple_form = Group(Opt(hui_finger_phrase) + Opt(special_finger) + xian_finger_phrase).set_results_name('SF')
   # complex form must have all
-  complex_form = Group(complex_finger + left_sub_phrase + right_sub_phrase).set_results_name('complex_form')
+  complex_form = Group(complex_finger + left_sub_phrase + right_sub_phrase).set_results_name('CF')
   # similar for aside_form
-  aside_form = Group(Opt(modifier) + Opt(special_finger) + move_finger_phrase).set_results_name('aside_form')
+  aside_form = Group(Opt(modifier) + Opt(special_finger) + move_finger_phrase).set_results_name('AF')
 
   # 谱字, lazy matching, order is important
   PUZI = complex_form | marker | both_finger | aside_form | simple_form
@@ -71,55 +69,56 @@ class AbbrParseVar:
   FEN = ['一','二','三','四','五','六','七','八','九','半']
   HUI_FINGER = ['大','食','中','名','跪','散']
   # Define individual components
-  hui = (ZeroOrMore((one_of(HUI) + Opt(one_of(FEN))) | '外')).set_results_name('number')
-  xian = (ZeroOrMore(one_of(XIAN))).set_results_name('number')
-  hui_finger = one_of(HUI_FINGER).set_results_name('finger')
-  xian_finger = one_of(XIAN_FINGER).set_results_name('finger')
-  move_finger = one_of(MOVE_FINGER).set_results_name('finger')
-  special_finger = one_of(SPECIAL_FINGER).set_results_name('special_finger')
-  modifier = one_of(MODIFIER).set_results_name('modifier')
-  both_finger = one_of(BOTH_FINGER).set_results_name('both_finger')
-  complex_finger = one_of(COMPLEX_FINGER).set_results_name('complex_finger')
+  hui = (ZeroOrMore((one_of(HUI) + Opt(one_of(FEN))) | '外')).set_results_name('hn')
+  xian = (ZeroOrMore(one_of(XIAN))).set_results_name('xn')
+  hui_finger = one_of(HUI_FINGER).set_results_name('hf')
+  xian_finger = one_of(XIAN_FINGER).set_results_name('xf')
+  move_finger = one_of(MOVE_FINGER).set_results_name('mf')
+  special_finger = one_of(SPECIAL_FINGER).set_results_name('sf')
+  modifier = one_of(MODIFIER).set_results_name('mod')
+  complex_finger = one_of(COMPLEX_FINGER).set_results_name('cf')
+  # TODO: should merge both_finger and marker into one sTandalone Form called 'TF'
+  both_finger = one_of(BOTH_FINGER).set_results_name('bf')
   marker = one_of(MARKER).set_results_name('marker')
 
   # Define phrase patterns
-  hui_finger_phrase = Group(hui_finger + hui).set_results_name('hui_finger_phrase')
-  xian_finger_phrase = Group(xian_finger + xian).set_results_name('xian_finger_phrase')
+  hui_finger_phrase = Group(hui_finger + hui).set_results_name('hfp')
+  xian_finger_phrase = Group(xian_finger + xian).set_results_name('xfp')
 
   # reduced_xian_finger phrase, which is a simplified version of xian_finger_phrase
   # only used in complex from
   # since the complex_finger is actually the xian_finger 
-  reduced_xian_finger_phrase = Group(xian).set_results_name('xian_finger_phrase')
-  left_sub_phrase = Group(hui_finger_phrase + Opt(special_finger) + reduced_xian_finger_phrase).set_results_name('left_sub_phrase')
-  right_sub_phrase = Group(hui_finger_phrase + Opt(special_finger) + reduced_xian_finger_phrase).set_results_name('right_sub_phrase')
+  reduced_xian_finger_phrase = Group(xian).set_results_name('xfp')
+  left_sub_phrase = Group(hui_finger_phrase + Opt(special_finger) + reduced_xian_finger_phrase).set_results_name('lsp')
+  right_sub_phrase = Group(hui_finger_phrase + Opt(special_finger) + reduced_xian_finger_phrase).set_results_name('rsp')
 
   # move_finger_phrase is a similar to hui_finger_phrase, used in aside form
-  move_finger_phrase = Group(move_finger + hui).set_results_name('move_finger_phrase')
+  move_finger_phrase = Group(move_finger + hui).set_results_name('mfp')
 
 
   # Define form pattern
   # simple form must have xian_finger_phrase, while hui_finger_phrase is optional
-  simple_form = Group(Opt(hui_finger_phrase) + Opt(special_finger) + xian_finger_phrase).set_results_name('simple_form')
+  simple_form = Group(Opt(hui_finger_phrase) + Opt(special_finger) + xian_finger_phrase).set_results_name('SF')
   # complex form must have all
-  complex_form = Group(complex_finger + left_sub_phrase + right_sub_phrase).set_results_name('complex_form')
+  complex_form = Group(complex_finger + left_sub_phrase + right_sub_phrase).set_results_name('CF')
   # similar for aside_form
-  aside_form = Group(Opt(modifier) + Opt(special_finger) + move_finger_phrase).set_results_name('aside_form')
+  aside_form = Group(Opt(modifier) + Opt(special_finger) + move_finger_phrase).set_results_name('AF')
 
   # 谱字, lazy matching, order is important
   PUZI = complex_form | marker | both_finger | aside_form | simple_form
 
 
-def parse(s: str, form = Literal['abbr','ortho']) -> Note:
+def parse(s: str, form : Literal['abbr','ortho'] = 'abbr'):
   match form:
     case 'abbr':
       d = AbbrParseVar.PUZI.parse_string(s).as_dict()
       # patch
-      if 'complex_form' in d:
-        # pop the miss matched number from hui_finger_phrase to xian_finger_phrase
-        left_xian_number = d['complex_form']['left_sub_phrase']['xian_finger_phrase']['number']
-        left_hui_number = d['complex_form']['left_sub_phrase']['hui_finger_phrase']['number']
-        right_xian_number = d['complex_form']['right_sub_phrase']['xian_finger_phrase']['number']
-        right_hui_number = d['complex_form']['right_sub_phrase']['hui_finger_phrase']['number']
+      if 'CF' in d:
+        # pop the mismatched number from hui_finger_phrase to xian_finger_phrase
+        left_xian_number = d['CF']['lsp']['xfp']['xn']
+        left_hui_number = d['CF']['lsp']['hfp']['hn']
+        right_xian_number = d['CF']['rsp']['xfp']['xn']
+        right_hui_number = d['CF']['rsp']['hfp']['hn']
         if len(left_xian_number) == 0:
           # case 1: mismatch e.g. ['十一'] instead of ['十'],['一']
           if (len(left_hui_number)) == 1 & (len(left_hui_number[0]) == 2):
@@ -139,70 +138,83 @@ def parse(s: str, form = Literal['abbr','ortho']) -> Note:
 
     case 'ortho':
       d = OrthoParseVar.PUZI.parse_string(s).as_dict()
+
     case _:
       raise ValueError(f"form must be either 'abbr'or 'ortho'")
-  pp.pprint(d)
 
-  return Note.from_dict(d)
+  def remove_chars_from_dict(d, chars):
+    if isinstance(d, dict):
+        return {k: remove_chars_from_dict(v, chars) for k, v in d.items()}
+    elif isinstance(d, list):
+        return [remove_chars_from_dict(item, chars) for item in d]
+    elif isinstance(d, str):
+        return d.translate({ord(c): None for c in chars})
+    else:
+        return d
 
-def lint(s: str) -> bool:
-  return NotImplementedError
+  d = remove_chars_from_dict(d, '徽分弦指音')
 
-def transcribe(s: str) -> str:
-  """简写法转译为标准法
-  """
-  # 简单替换
-  d = {
-    '大': '大指',
-    '食': '食指',
-    '中': '中指',
-    '名': '名指',
-    '跪': '跪指',
-    '散': '散音',
-    '外': '徽外',
-  }
+  return ParseNode.from_dict(d)
 
-  # 撮式
-  if s[0] in COMPLEX_FINGER:
+@dataclass
+class ParseNode:
+    label: str
+    value: str
+    children: List["ParseNode"] = field(default_factory=list)
 
-    left = s[:i+2]
-    right = s[i+2:]
-  # 旁注形式
-  # 简单形式
-  # 添加徽分弦
-  for finger in XIAN_FINGER:
-    i = s.find(finger)
-    if i != -1:
-      hui, xian = s[:i], s[i:]
-      xian_finger, xian_number = xian[0:len(finger)], xian[len(finger):]
-      if hui != '':
-        if hui[-1] in ['绰','注']:
-          special_finger = hui[-1]
-          hui = hui[:-1]
-        else:
-          special_finger = ''
-        hui_finger, hui_number = hui[0], hui[1:]
-        hui_finger = d[hui_finger]
-        if len(hui_number) == 1:
-          if hui_number == '外':
-            hui_number = '徽' + hui_number
-          else:
-            hui_number = hui_number + '徽'
-        elif len(hui_number) == 2:
-          if hui_number[0:2] in ['十一', '十二', '十三']:
-            hui_number += '徽'
-          else:
-            hui_number = hui_number[0] + '徽' + hui_number[1] + '分'
-        elif len(hui_number) == 3:
-          hui_number = hui_number[0:2] + '徽' + hui_number[2] + '分'
-        else:
-          pass
-      else:
-        hui_finger = ''
-        hui_number = ''
-        special_finger = ''
-      xian_number = ''.join(n+'弦' for n in xian_number)
-      t = hui_finger + hui_number + special_finger + xian_finger + xian_number
-      break
-  
-  return t
+    def is_leaf(self) -> bool:
+        return not self.children
+
+    def to_string(self, form: Literal['abbr', 'ortho'] = 'abbr') -> str:
+        values: List[str] = []
+    
+        def traverse(node: "ParseNode") -> None:
+            if node.is_leaf():
+                label = node.label
+                value = node.value
+                if form == 'ortho':
+                    if label == 'hf':
+                        if value in ['大', '食', '中', '名', '跪']:
+                            value = value + '指'
+                        elif value == '散':
+                            value = '散音'
+                    elif label == 'hn1':
+                        if value == '外':
+                            value = '徽外'
+                        elif value in ['一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二', '十三']:
+                            value = value + '徽'
+                    elif label == 'hn2':
+                        value = value + '分'
+                    elif label.startswith('xn'):
+                        value = value + '弦'
+                values.append(value)
+            for child in node.children:
+                traverse(child)
+    
+        traverse(self)
+        return ''.join(values)
+
+    @classmethod
+    def from_dict(cls, d: dict) -> "ParseNode":
+        assert len(d) == 1, "The dict should only have one key from 'SF', 'CF', 'AF', 'TF'"
+        label, content = next(iter(d.items()))
+        return cls._build_node(label, content)
+
+    @classmethod
+    def _build_node(cls, label: str, content) -> "ParseNode":
+        if isinstance(content, str):
+            return cls(label=label, value=content)
+        if isinstance(content, dict):
+            node = cls(label=label, value='')
+            for key, val in content.items():
+                if isinstance(val, list):
+                    for i, item in enumerate(val, start=1):
+                        child = cls(
+                            label=f"{key}{i}",
+                            value=item
+                        )
+                        node.children.append(child)
+                else:
+                    node.children.append(cls._build_node(key, val))
+            return node
+        raise TypeError(f"Unsupported node content type: {type(content)}")
